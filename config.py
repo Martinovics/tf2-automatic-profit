1cb253011ebecdea0881b9e2cff3c7705f90cf51
Martinovics <40437323+Martinovics@users.noreply.github.com>
2020-07-02 13:06:06 +0200

    updated gitignore and config

--- config.py
+++ config.py
@@ -2,15 +2,16 @@ config = {
     'key_price': 55,  # actual key price in ref (float)
 
     # the bot's admins (list of strings); if you don't set all admins it will crash :/
-    'admins': ['76561198123935354', '76561198951577586'],
+    'admins': ['steamID64', 'steamID64', '...'],
 
     # the directory's path, which has polldata.json and pricelist.json in it
-    'files_path': ['C:\\Users\\balaz\\Desktop\\Programs\\JS\\tf2-automatic-master\\files\\providipresents'],
+    'files_path': ['/home/user/tf2-automatic-master/files/accountNamedDirectory',
+                   'C:\\Users\\user\\tf2-automatic-master\\files\\accountNamedDirectory'],
 
     'most_profit_items_count': 5,  # print x of the most profitable items
     'least_profit_items_count': 5,    # print x of the least profitable items
 
     # b1.76k240620_232009 > b 1.76k 24/06/20 23:20:09 >>> bought for 1.76 keys at 23:20:09 on 24th June(06) 2020
     # s1.78k250620_104324 > s 1.78k 25/06/20 10:43:24 >>> sold for 1.78 keys at 10:43:24 on 25th June(06) 2020
-    'print_item_data': True  # set this to True to print some info of the items
+    'print_item_data': False  # set this to True to print some info of the items
 }
