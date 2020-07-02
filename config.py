9d89552b5917a29a6b9f9b4297842b2701c2d017
Martinovics <40437323+Martinovics@users.noreply.github.com>
2020-07-02 14:42:23 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ config = {
 
     # b1.76k240620_232009 > b 1.76k 24/06/20 23:20:09 >>> bought for 1.76 keys at 23:20:09 on 24th June(06) 2020
     # s1.78k250620_104324 > s 1.78k 25/06/20 10:43:24 >>> sold for 1.78 keys at 10:43:24 on 25th June(06) 2020
-    'print_item_data': True  # set this to True to print some info of the items
+    'print_item_data': False  # set this to True to print some info of the items
 }
