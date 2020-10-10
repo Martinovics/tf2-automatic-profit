5db4f1941a5078b669e5e4b11adabdfb79b6054e
Martinovics <40437323+Martinovics@users.noreply.github.com>
2020-10-10 22:43:50 +0200

    Update config_temp.py

--- tools/config_temp.py
+++ tools/config_temp.py
@@ -9,10 +9,10 @@ class Config:
     ADMINS: List[str] = []
     PATHS: List[str] = []
 
-    MOST_PRIFIT_ITEMS_COUNT = 5   # print x amount of the most profitable items
-    LEAST_PRIFIT_ITEMS_COUNT = 5  # print x amount of the least profitable items
-    MOST_TRADED_ITEMS_COUNT = 5   # print x amount of the most traded items
-    LEAST_PRIFIT_ITEMS_COUNT = 5  # print x amount of the least traded items
+    MOST_PRIFIT_ITEMS_COUNT: int = 5   # print x amount of the most profitable items
+    LEAST_PRIFIT_ITEMS_COUNT: int = 5  # print x amount of the least profitable items
+    MOST_TRADED_ITEMS_COUNT: int = 5   # print x amount of the most traded items
+    LEAST_PRIFIT_ITEMS_COUNT: int = 5  # print x amount of the least traded items
 
     PRINT_ALL_TRADES: bool = False
 
@@ -20,4 +20,4 @@ class Config:
 
 
 class Const:
-    CURRENCIES: List[str] = ['5021;6', '5002;6', '5001;6', '5000;6']  # key, ref, rec, scrap
+    CURRENCIES: dict = {'5021;6': Config.KEY_PRICE * 9, '5002;6': 9, '5001;6': 3, '5000;6': 1}  # key, ref, rec, scrap
