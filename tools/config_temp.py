5ecf8293f8fc87d7d25d5ed0665d9b4724f61ca5
Martinovics <40437323+Martinovics@users.noreply.github.com>
2020-10-12 17:24:31 +0200

    Update config_temp.py

--- tools/config_temp.py
+++ tools/config_temp.py
@@ -7,12 +7,15 @@ class Config:
     KEY_PRICE: float = 45
 
     ADMINS: List[str] = []
-    PATHS: List[str] = []
+    PATHS: List[str] = []  # like "C:/tf2autobot-master/files/botusername" or like "/home/tf2autobot-master/files/botusername"
 
     MOST_PRIFIT_ITEMS_COUNT: int = 5   # print x amount of the most profitable items
     LEAST_PRIFIT_ITEMS_COUNT: int = 5  # print x amount of the least profitable items
+    '''
+    MOST_PRIFIT_ITEMS_72H: int = 5     # print x amount of the most profitable items over 72H
     MOST_TRADED_ITEMS_COUNT: int = 5   # print x amount of the most traded items
-    LEAST_PRIFIT_ITEMS_COUNT: int = 5  # print x amount of the least traded items
+    LEAST_TRADED_ITEMS_COUNT: int = 5  # print x amount of the least traded items
+    '''
 
     PRINT_ALL_TRADES: bool = False
 
