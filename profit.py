68729a85c27fb093f63c1f68fdd09136a4da2df6
Martinovics <40437323+Martinovics@users.noreply.github.com>
2021-03-29 22:21:07 +0200

    Update profit.py

--- profit.py
+++ profit.py
@@ -1,23 +1,52 @@
+import json
+import asyncio
 import aiofiles
+
+
 
 
 
 
 class Profit:
 
-    def __init__(self, history_path):
+    def __init__(self, history_path: str):
         self.path = history_path
-
-        return None
-
+        self.polldata: list = None
+        self.pricelist: list = None
 
 
 
     @classmethod
-    async def foo():
-        pass
+    async def read_history(self) -> dict:
+
+        async with aiofiles.open(self.path, mode='r', encoding='utf-8') as f:
+            data = await f.read()
+
+        for _ in range(5):
+            try:
+                return json.loads(data)
+            except json.decoder.JSONDecodeError:
+                asyncio.sleep(0.1)
+        else:
+            raise Exception(f"Couldn't load {self.path}")
 
 
 
 
-a = Profit(history_path='./asd')
+    async def initialize(self):
+        self.history = await self.read_history()
+
+        # reduce the size of the history
+
+
+        self.polldata = []
+        if 'offerData' in self.polldata:
+            self.polldata = self.polldata['offerData']
+
+
+
+
+
+
+
+profit = Profit(history_path='./tests/providipresents')
